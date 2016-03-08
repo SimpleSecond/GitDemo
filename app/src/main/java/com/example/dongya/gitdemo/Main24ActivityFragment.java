1eeb0a38645e71bb3a54e06800d782aab9fd5ca3
SimpleSecond <simplsecond@163.com>
2016-03-08 08:50:17 +0800

    测试用例6 添加含有Fragment的Activity

---
+++ app/src/main/java/com/example/dongya/gitdemo/Main24ActivityFragment.java
@@ -0,0 +1,22 @@
+package com.example.dongya.gitdemo;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class Main24ActivityFragment extends Fragment {
+
+    public Main24ActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main24, container, false);
+    }
+}
